3d36ab89fde138d8d925c4c38346de7f061dcd05
Govind S R <64773624+theinfinox@users.noreply.github.com>
2024-06-09 04:57:59 +0530

    Update dht_only_json.ino

--- dht_only_json.ino
+++ dht_only_json.ino
@@ -3,15 +3,16 @@
 #include <DHT.h>
 #include <time.h> // Include the time library
 
-#define DHTPIN D3          // What digital pin the DHT11 is connected to
-#define DHTTYPE DHT11      // Specify using DHT11
+#define DHTPIN D3           // What digital pin the DHT11 is connected to
+#define DHTTYPE DHT11       // Specify using DHT11
+#define airquality_PIN A0   // Analog pin for airquality sensor
+
 DHT dht(DHTPIN, DHTTYPE);
+ESP8266WebServer server(80); // Set HTTP server on port 80
 
 const char* ssid = "pi";
 const char* password = "raspberry";
 const char* ntpServer = "pool.ntp.org"; // NTP server
-
-ESP8266WebServer server(81); // Set HTTP server on port 81
 
 void setup() {
   Serial.begin(115200);
@@ -26,7 +27,9 @@ void setup() {
 
   configTime(0, 0, ntpServer); // Configure the NTP server
 
-  server.on("/", HTTP_GET, handleRoot);
+  pinMode(airquality_PIN, INPUT); // Set airquality pin as input
+  server.on("/", HTTP_GET, handleRoot); // Root endpoint
+  server.on("/data", HTTP_GET, handleData); // Data endpoint
   server.begin();
   Serial.println("HTTP server started");
 
@@ -38,20 +41,25 @@ void loop() {
 }
 
 void handleRoot() {
+  server.send(200, "text/plain", "Welcome to the ESP8266 Air Quality Monitor");
+}
+
+void handleData() {
   float temp = dht.readTemperature(); // Read temperature as Celsius
   float hum = dht.readHumidity(); // Read humidity (percentage)
+  int airqualityValue = analogRead(airquality_PIN); // Read air quality level from the analog pin
+  float airqualityPercentage = ((1023 - airqualityValue) / 1023.0) * 100.0; // Calculate air quality percentage inversely
 
   time_t now = time(nullptr);
   struct tm *tm_struct = gmtime(&now);
-
   char utcString[25];
   strftime(utcString, sizeof(utcString), "%Y-%m-%d %H:%M:%S", tm_struct);
   
   if (isnan(temp) || isnan(hum)) {
-    server.send(500, "application/json", "{\"error\":\"Failed to read from DHT sensor\"}");
+    server.send(500, "application/json", "{\"error\":\"Failed to read from sensors\"}");
   } else {
-    char json[200];
-    snprintf(json, sizeof(json), "{\"temperature\": %.2f, \"humidity\": %.2f, \"utc\": \"%s\"}", temp, hum, utcString);
+    char json[300];
+    snprintf(json, sizeof(json), "{\"temperature\": %.2f, \"humidity\": %.2f, \"airquality\": %.2f, \"utc\": \"%s\"}", temp, hum, airqualityPercentage, utcString);
     server.send(200, "application/json", json);
   }
 }
